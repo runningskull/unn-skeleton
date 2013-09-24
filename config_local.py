ba148e7d5d764beb1823faa6f8da19f0cd2a2abd
J.R. Patten <jr@juanpatten.com>
2013-09-23 21:22:07 -0600

    Initial Commit

---
+++ config_local.py
@@ -0,0 +1,6 @@
+# Change this:
+deploy_command = 's3cmd put --recursive _public/ s3://www.MY_SITE.com'
+
+# These are the defaults. Change them to suit you, or remove them.
+editor = 'vim {}'
+compass_path = '/Library/Ruby/Gems/1.8/gems/compass-0.12.2/frameworks/compass/stylesheets'
